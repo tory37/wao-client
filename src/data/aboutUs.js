57f30541372a79a05f119ed4a5c01ca724fe9f4f
Tory Hebert <tory37@gmail.com>
2019-08-18 14:30:36 -0500

    about us additions

--- src/data/aboutUs.js
+++ src/data/aboutUs.js
@@ -2,7 +2,7 @@ export default {
 	founders: [
 		{
 			name: 'Tory aka Birb',
-			imageUrl: 'http://drive.google.com/uc?export=view&id=1rkJKBznDbGqrb83wNrF4YoB3OuxjuS0W',
+			imageUrl: 'http://drive.google.com/uc?export=view&id=1HSnGTtWFoBgtxpZx5KI1vaQpJNdTdia1',
 			roles: 'Podcast Host | Software Engineer | Facebook Mod | Discord Admin',
 			description: 'Tory is a software engineer, drummer, weight lifter, and all out weeb. He enjoys dark, cold rooms where he can cover up and code, watch tv, play games or listen music.',
 			stats: [
@@ -18,8 +18,8 @@ export default {
 			iconName: 'fas fa-dove'
 		},
 		{
-			name: 'Brandon',
-			imageUrl: 'http://drive.google.com/uc?export=view&id=1bkbpvdsZ3XRriH-11NtXyTACqfeXkbyQ',
+			name: 'Brandon aka Phyleus',
+			imageUrl: 'http://drive.google.com/uc?export=view&id=1HPreGA6C_87XpNP7OAXjxC9vhxeCS4W_',
 			roles: 'Podcast Host | Video Editor | Facebook Mod | Discord Admin',
 			description: 'Brandon is a drummer, singer, bio, bio bio ',
 			stats: [
@@ -41,10 +41,10 @@ export default {
 	],
 	contributors: [
 		{
-			name: 'Trent',
+			name: 'Trent aka Lord Blackwool',
 			imageUrl: 'https://vignette.wikia.nocookie.net/megamitensei/images/3/3f/Incubus2.jpg/revision/latest?cb=20190331170439',
 			roles: 'Art Engineer | Discord Host',
-			description: 'Tell em what it is BOII',
+			description: 'gimme da bio',
 			stats: [
 				{
 					title: 'Fav Anime',
@@ -58,10 +58,11 @@ export default {
 			iconName: 'fas fa-paint-brush'
 		},
 		{
-			name: 'Landon',
+			name: 'Landon aka Landon',
 			imageUrl: 'https://i.kym-cdn.com/entries/icons/original/000/004/952/emo-dad-22891-1257438303-107.jpg',
-			roles: 'Audio Engineer |',
-			description: 'Watup its ya boi',
+			roles: 'Audio Engineer | Composer',
+			description:
+				'Landon is an audio engineer but who cares about that. He is currently writing a dissertation about how birds are government drones. He expects to graduate from LSU with a PhD in conspiracy theories in the spring on 2020. Most importantly he is ranked in the top 1% of Modern Warfare players (not even joking).',
 			stats: [
 				{
 					title: 'Fav Anime',
@@ -73,6 +74,40 @@ export default {
 				}
 			],
 			iconName: 'fas fa-music'
+		},
+		{
+			name: 'Ty aka Moaku',
+			imageUrl: 'https://img1.ak.crunchyroll.com/i/spire4/dbac8331314d177bf9b847e84a0470931559392436_full.jpg',
+			roles: 'Discord Admin |',
+			description: 'gimme the bio',
+			stats: [
+				{
+					title: 'Fav Anime',
+					value: 'Fullmetal Alchemist'
+				},
+				{
+					title: 'Fav Food',
+					value: 'Curry'
+				}
+			],
+			iconName: 'fas fa-tree'
+		},
+		{
+			name: 'Steffi aka Ylarnja',
+			imageUrl: 'https://vignette.wikia.nocookie.net/ninokuni/images/6/65/Lofty.png/revision/latest?cb=20190405233503',
+			roles: 'Discord Admin |',
+			description: 'Gimme da bio',
+			stats: [
+				{
+					title: 'Fav Anime',
+					value: 'Fullmetal Alchemist'
+				},
+				{
+					title: 'Fav Food',
+					value: 'Curry'
+				}
+			],
+			iconName: 'fas fa-rocket'
 		}
 	]
 };
