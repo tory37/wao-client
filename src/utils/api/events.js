502d341a85b89cfec2e5e079042eda7eed0added
Tory Hebert <tory37@gmail.com>
2019-09-22 17:29:35 -0500

    No upcoming events banner and timeout on events fetch

--- src/utils/api/events.js
+++ src/utils/api/events.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 
 const fetchAllEvents = () => {
 	const url = `${process.env.REACT_APP_API_URL}/events`;
-	return axios.get(url);
+	return axios.get(url, {timeout: 5000});
 }
 
 const fetchFutureEvents = () => {
 	const url = `${process.env.REACT_APP_API_URL}/events/future`;
-	return axios.get(url);
+	return axios.get(url, {timeout: 5000});
 };
 
 const fetchPastEvents = () => {
 	const url = `${process.env.REACT_APP_API_URL}/events/past`;
-	return axios.get(url);
+	return axios.get(url, {timeout: 5000});
 };
 
 const createEvent = eventData => {
