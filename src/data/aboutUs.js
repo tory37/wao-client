440c1e2952b1d2cb0ee0b48bc5440dabb9b8fb44
Tory Hebert <tory37@gmail.com>
2020-01-15 17:40:58 -0600

    Added iron to about page

--- src/data/aboutUs.js
+++ src/data/aboutUs.js
@@ -1,113 +1,136 @@
 export default {
-	founders: [
-		{
-			name: 'Tory aka Birb',
-			imageUrl: 'tory.jpg',
-			roles: 'Podcast Host | Software Engineer | Facebook Group Admin | Discord Admin',
-			description: 'Tory is a software engineer, drummer, weight lifter, and all out weeb. He enjoys dark, cold rooms where he can cover up and code, watch tv, play games or listen music.  https://twitter.com/torayquaza',
-			stats: [
-				{
-					title: 'Fav Anime',
-					value: 'Fullmetal Alchemist'
-				},
-				{
-					title: 'Fav Food',
-					value: 'Curry'
-				}
-			],
-			iconName: 'fas fa-dove'
-		},
-		{
-			name: 'Brandon aka Sensei Swagg',
-			imageUrl: 'brandon.jpg',
-			roles: 'Podcast Host | Video/Photo Editor | FB Group Admin | Discord Admin',
-			description: 'Brandon is an Agile Coach by day and a drummer/singer at night. He enjoys hanging out with his buds, watching/discussing anime, and playing video games.',
-			stats: [
-				{
-					title: 'Fav Anime',
-					value: 'Naruto Girl'
-				},
-				{
-					title: 'Fav Food',
-					value: 'Curry'
-				},
-				{
-					title: 'Atk',
-					value: '128'
-				}
-			],
-			iconName: 'fas fa-drum'
-		}
-	],
-	contributors: [
-		{
-			name: 'Alistar Blackwool',
-			imageUrl: 'trent.jpg',
-			roles: 'Art Engineer | Discord Host',
-			description: 'Lord of nothing and constant menace to society',
-			stats: [
-				{
-					title: 'Fav Anime',
-					value: 'Something Wierd'
-				},
-				{
-					title: 'Fav Food',
-					value: 'Curry'
-				}
-			],
-			iconName: 'fas fa-paint-brush'
-		},
-		{
-			name: 'Landon aka Landon',
-			imageUrl: 'landon.jpg',
-			roles: 'Audio Engineer | Composer',
-			description:
-				'Landon is an audio engineer but who cares about that. He is currently writing a dissertation about how birds are government drones. He expects to graduate from LSU with a PhD in conspiracy theories in the spring on 2020. Most importantly he is ranked in the top 1% of Modern Warfare players (not even joking).',
-			stats: [
-				{
-					title: 'Fav Anime',
-					value: 'Fullmetal Alchemist'
-				},
-				{
-					title: 'Fav Food',
-					value: 'Curry'
-				}
-			],
-			iconName: 'fas fa-music'
-		},
-		{
-			name: 'Moaku',
-			imageUrl: 'moaku.png',
-			roles: 'Discord Admin |',
-			description: 'Nerd, been studying Japanese for like 4 years non stop, and correctly believes that Hibike! Euphonium is objectively the best anime to ever exist. He also runs https://www.moakuwolf.moe/ where you can read up on Japanese used in anime and games!',
-			stats: [
-				{
-					title: 'Fav Anime',
-					value: 'Fullmetal Alchemist'
-				},
-				{
-					title: 'Fav Food',
-					value: 'Curry'
-				}
-			],
-			iconName: 'fas fa-tree'
-		},
-		{
-			name: 'Steffi aka Ylarnja',
-			imageUrl: 'steffi.jpeg',
-			roles: 'Discord Admin |',
-			description: 'Software Developer, japanese learner and professional sloth.',
-			stats: [
-				{
-					title: 'Fav Anime',
-					value: 'Fullmetal Alchemist'
-				},
-				{
-					title: 'Fav Food',
-					value: 'Curry'
-				}
-			],
-			iconName: 'fas fa-rocket'
-		}
-	]
+  founders: [
+    {
+      name: "Tory aka Birb",
+      imageUrl: "tory.jpg",
+      roles:
+        "Podcast Host | Software Engineer | Facebook Group Admin | Discord Admin",
+      description:
+        "Tory is a software engineer, drummer, weight lifter, and all out weeb. He enjoys dark, cold rooms where he can cover up and code, watch tv, play games or listen music.  https://twitter.com/torayquaza",
+      stats: [
+        {
+          title: "Fav Anime",
+          value: "Fullmetal Alchemist"
+        },
+        {
+          title: "Fav Food",
+          value: "Curry"
+        }
+      ],
+      iconName: "fas fa-dove"
+    },
+    {
+      name: "Brandon aka Sensei Swagg",
+      imageUrl: "brandon.jpg",
+      roles:
+        "Podcast Host | Video/Photo Editor | FB Group Admin | Discord Admin",
+      description:
+        "Brandon is an Agile Coach by day and a drummer/singer at night. He enjoys hanging out with his buds, watching/discussing anime, and playing video games.",
+      stats: [
+        {
+          title: "Fav Anime",
+          value: "Naruto Girl"
+        },
+        {
+          title: "Fav Food",
+          value: "Curry"
+        },
+        {
+          title: "Atk",
+          value: "128"
+        }
+      ],
+      iconName: "fas fa-drum"
+    }
+  ],
+  contributors: [
+    {
+      name: "Alistar Blackwool",
+      imageUrl: "trent.jpg",
+      roles: "Art Engineer | Discord Host",
+      description: "Lord of nothing and constant menace to society",
+      stats: [
+        {
+          title: "Fav Anime",
+          value: "Something Wierd"
+        },
+        {
+          title: "Fav Food",
+          value: "Curry"
+        }
+      ],
+      iconName: "fas fa-paint-brush"
+    },
+    {
+      name: "Landon aka Landon",
+      imageUrl: "landon.jpg",
+      roles: "Audio Engineer | Composer",
+      description:
+        "Landon is an audio engineer but who cares about that. He is currently writing a dissertation about how birds are government drones. He expects to graduate from LSU with a PhD in conspiracy theories in the spring on 2020. Most importantly he is ranked in the top 1% of Modern Warfare players (not even joking).",
+      stats: [
+        {
+          title: "Fav Anime",
+          value: "Fullmetal Alchemist"
+        },
+        {
+          title: "Fav Food",
+          value: "Curry"
+        }
+      ],
+      iconName: "fas fa-music"
+    },
+    {
+      name: "Moaku",
+      imageUrl: "moaku.png",
+      roles: "Discord Admin |",
+      description:
+        "Nerd, been studying Japanese for like 4 years non stop, and correctly believes that Hibike! Euphonium is objectively the best anime to ever exist. He also runs https://www.moakuwolf.moe/ where you can read up on Japanese used in anime and games!",
+      stats: [
+        {
+          title: "Fav Anime",
+          value: "Fullmetal Alchemist"
+        },
+        {
+          title: "Fav Food",
+          value: "Curry"
+        }
+      ],
+      iconName: "fas fa-tree"
+    },
+    {
+      name: "Steffi aka Ylarnja",
+      imageUrl: "steffi.jpeg",
+      roles: "Discord Admin |",
+      description:
+        "Software Developer, japanese learner and professional sloth.",
+      stats: [
+        {
+          title: "Fav Anime",
+          value: "Fullmetal Alchemist"
+        },
+        {
+          title: "Fav Food",
+          value: "Curry"
+        }
+      ],
+      iconName: "fas fa-rocket"
+    },
+    {
+      name: "Ironsouled",
+      imageUrl: "iron.jpg",
+      roles: "Discord Admin |",
+      description: "Business owner, VN fanatic, and the saltiest man alive",
+      stats: [
+        {
+          title: "Fav Anime",
+          value: "Fullmetal Alchemist"
+        },
+        {
+          title: "Fav Food",
+          value: "Curry"
+        }
+      ],
+      iconName: "fas fa-feather"
+    }
+  ]
 };
